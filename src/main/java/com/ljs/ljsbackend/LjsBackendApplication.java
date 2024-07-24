f5ec47d97544c9da753e85283593c882b52093c2
js.lee <js.lee@brycenkorea.co.kr>
2024-07-24 22:49:13 +0900

    프록시 빠꾸

--- src/main/java/com/ljs/ljsbackend/LjsBackendApplication.java
+++ src/main/java/com/ljs/ljsbackend/LjsBackendApplication.java
@@ -13,21 +13,21 @@ public class LjsBackendApplication {
         SpringApplication.run(LjsBackendApplication.class, args);
     }
 
-//front proxy 설정으로 대체
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedMethods("*")
-//                        .allowedOrigins("http://58.148.100.28","http://localhost:5000","http://127.0.0.1:5000","http://172.30.1.19:5000","http://192.168.219.111:5000")
-//                        .allowCredentials(false)
-//                        .maxAge(3000);
-//                //registry.addMapping("/**").allowedOriginPatterns("*");
-//
-//            }
-//        };
-//    }
+//front proxy 설정으로 대체 프록시 설정하는게 오류가 있는듯
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://58.148.100.28","http://localhost:5000","http://127.0.0.1:5000","http://172.30.1.19:5000","http://192.168.219.111:5000")
+                        .allowCredentials(false)
+                        .maxAge(3000);
+                //registry.addMapping("/**").allowedOriginPatterns("*");
+
+            }
+        };
+    }
 
 }
